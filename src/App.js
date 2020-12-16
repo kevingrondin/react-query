0d1395da50b30912af9b027a92b91e0a10f81684
Kevin GRONDIN <kevin.grondin@reuniwatt.com>
2020-12-16 18:42:32 +0400

    react-query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery, useQueryClient, useMutation } from 'react-query';
 
-function App() {
+const products = [
+  {id: 1, name: 'chaussure', price: 120 },
+  {id: 2, name: 'tee-shirt', price: 18 },
+  {id: 3, name: 'pantalon', price: 48 },
+]
+
+const postProduct = (params) => {
+  setTimeout(()=> {
+    const newProduct = {
+      id: products.length+1,
+      name: params.name,
+      price: params.price,
+    }
+
+    products.push(newProduct)
+    return newProduct
+  }, 2000)
+}
+
+const getProducts = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=> {
+      resolve(products)
+    }, 4000)
+  })
+}
+
+const App = () => {
+
+  const queryClient = useQueryClient()
+
+  const [ name, setName ] = useState('')
+  const [ price, setPrice ] = useState(0)
+
+  const { data, error, isFetching } = useQuery('products', getProducts)
+  const addProduct = useMutation(postProduct, {
+    onSuccess: () => queryClient.invalidateQueries('products')
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        !isFetching && data ? (
+          <>
+            <ul>{data.map(x => (<li key={x.id}>{ x.name }</li>) )}</ul>
+            <p>
+              Name: <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+            </p>
+            <p>
+              Price: <input type="text" value={price} onChange={(e)=>setPrice(e.target.value)}/>
+            </p>
+            <p>
+              <button onClick={() => {
+                console.info('click')
+                addProduct.mutate({name, price})
+              }}>
+                add
+              </button>
+            </p>
+          </>
+        ) : <p>Fetching...</p>
+      }
+      {error !==null && <p>{error.message}</p>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query'
 import reportWebVitals from './reportWebVitals';
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
